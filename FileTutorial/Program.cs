0d264117277a7af011364036a471bd1ed73219ec
Hanna-Johansson-KB <Hanna.Johansson@amal.se>
2024-01-12 09:11:26 +0100

    added comments

--- FileTutorial/Program.cs
+++ FileTutorial/Program.cs
@@ -1,21 +1,31 @@
 ﻿class Program {
     public static void Main(string[] args) {
+        //define filepath to mydocuments
         string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"/test.txt";
         StreamWriter sw;
 
+        //check if file exists
         if (File.Exists(docPath)) {
+            //if yes create stream to add text
             sw = File.AppendText(docPath);
         } else {
+            //else create new file stream
             sw = File.CreateText(docPath);
         }
         string input = Console.ReadLine();
 
+        //Write to file
         sw.WriteLine(input);
 
+        //close stream to save changes
         sw.Close();
 
+
+        //Create stream to read file
         StreamReader sr = File.OpenText(docPath);
+        
         string docLine;
+        //read all lines in file
         while((docLine = sr.ReadLine()) != null) {
             Console.WriteLine(docLine);
         }
