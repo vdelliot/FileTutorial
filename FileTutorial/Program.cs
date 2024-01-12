1d03f692f81cc49f3396c8eb0011d3b088e9c244
Hanna-Johansson-KB <Hanna.Johansson@amal.se>
2024-01-12 09:08:56 +0100

    Add project files.

---
+++ FileTutorial/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program {
+    public static void Main(string[] args) {
+        string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"/test.txt";
+        StreamWriter sw;
+
+        if (File.Exists(docPath)) {
+            sw = File.AppendText(docPath);
+        } else {
+            sw = File.CreateText(docPath);
+        }
+        string input = Console.ReadLine();
+
+        sw.WriteLine(input);
+
+        sw.Close();
+
+        StreamReader sr = File.OpenText(docPath);
+        string docLine;
+        while((docLine = sr.ReadLine()) != null) {
+            Console.WriteLine(docLine);
+        }
+    }
+}
