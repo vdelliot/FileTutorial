1c7802f803caf8e5756c9250d9d518ab7cbdbc44
Hanna-Johansson-KB <Hanna.Johansson@amal.se>
2024-01-15 09:12:11 +0100

    Changed to save to json

--- FileTutorial/Program.cs
+++ FileTutorial/Program.cs
@@ -1,10 +1,13 @@
-﻿class Program {
+﻿using System.Text.Json;
+
+class Program {
     public static void Main(string[] args) {
         //define filepath to mydocuments
-        string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"/test.txt";
+        string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"/test.json";
         StreamWriter sw;
 
         //check if file exists
+        /*
         if (File.Exists(docPath)) {
             //if yes create stream to add text
             sw = File.AppendText(docPath);
@@ -12,10 +15,22 @@
             //else create new file stream
             sw = File.CreateText(docPath);
         }
-        string input = Console.ReadLine();
+        */
+        sw = File.CreateText(docPath);
+        List<ClassRoom> rooms = new List<ClassRoom>();
+        ClassRoom room = new ClassRoom("T2", 20);
+        rooms.Add(room);
+        room = new ClassRoom("T1", 32); 
+        rooms.Add(room);
+        room = new ClassRoom("T3", 16); 
+        rooms.Add(room);
+        JsonSerializerOptions option = new JsonSerializerOptions();
+        option.WriteIndented = true;
+        string json = JsonSerializer.Serialize(rooms, option);
+        //string input = Console.ReadLine();
 
         //Write to file
-        sw.WriteLine(input);
+        sw.WriteLine(json);
 
         //close stream to save changes
         sw.Close();
@@ -23,13 +38,30 @@
 
         //Create stream to read file
         StreamReader sr = File.OpenText(docPath);
-        
+        /*
         string docLine;
         //read all lines in file
         while((docLine = sr.ReadLine()) != null) {
             Console.WriteLine(docLine);
         }
+        */
+        string saveData = sr.ReadToEnd();
+        List<ClassRoom> temp = JsonSerializer.Deserialize<List<ClassRoom>>(saveData);
+        for(int i = 0; i < temp.Count; i++) {
+            Console.WriteLine(temp[i].Name + " " + temp[i].NrSeats);
+        }
 
+        //Console.WriteLine(saveData);
         sr.Close();
+    }
+
+    class ClassRoom {
+        public string Name { get; set; }
+        public int NrSeats { get; set; }
+
+        public ClassRoom(string name,int nrSeats) {
+            Name = name;
+            NrSeats = nrSeats;
+        }
     }
 }
